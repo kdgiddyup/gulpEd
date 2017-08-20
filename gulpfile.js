dd794e9722d0928ef68c89f5e0ece28b40f14423
Kelly Davis <kellydavis1974@gmail.com>
2017-08-20 18:12:32 -0400

    adding node glob pattern matching

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,12 @@ gulp.task('hello', function() {
     // Stuff here
     console.log("Hello, Kelly!");
   });
-  
+
 // let's try compiling sass
 // task is named "sass"
 //callback function returns source scss files compiled into css by the gulp-sass plug-in
 gulp.task("sass", function() {
-    return gulp.src("app/scss/styles.scss")
+    return gulp.src("scss/**/*.scss")
     .pipe(sass().on("error",sass.logError))
     .pipe(gulp.dest('app/css'));
 });
